32530ef11ab51a321516486d03562adc40ca31b4
Chelsea Farley <chelseafarley91@gmail.com>
2022-02-10 20:46:39 +1300

    Reward Ads

--- app/src/main/java/com/tripwiretech/admobadsdemo/MainActivity.kt
+++ app/src/main/java/com/tripwiretech/admobadsdemo/MainActivity.kt
@@ -3,16 +3,17 @@ package com.tripwiretech.admobadsdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.LoadAdError
-import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.*
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import com.google.android.gms.ads.rewarded.RewardItem
+import com.google.android.gms.ads.rewarded.RewardedAd
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
 
 class MainActivity : AppCompatActivity() {
-    private var _interstitialAd: InterstitialAd? = null;
-    private var _bannerAd: AdView? = null;
+    private var _interstitialAd: InterstitialAd? = null
+    private var _bannerAd: AdView? = null
+    private var _rewardAd: RewardedAd? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +21,22 @@ class MainActivity : AppCompatActivity() {
 
         MobileAds.initialize(this)
         loadInterstitial()
+        loadRewardAd()
         loadBannerAd()
+    }
+
+    private fun loadRewardAd() {
+        var adRequest = AdRequest.Builder().build()
+
+        RewardedAd.load(this, "ca-app-pub-3940256099942544/5224354917", adRequest, object : RewardedAdLoadCallback() {
+            override fun onAdFailedToLoad(p0: LoadAdError) {
+                _rewardAd = null
+            }
+
+            override fun onAdLoaded(p0: RewardedAd) {
+                _rewardAd = p0
+            }
+        })
     }
 
     private fun loadInterstitial() {
@@ -44,5 +60,20 @@ class MainActivity : AppCompatActivity() {
 
     fun showInterstitial(view: View) {
         _interstitialAd?.show(this)
+    }
+
+    fun showReward(view: View) {
+        _rewardAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+            override fun onAdDismissedFullScreenContent() {
+                _rewardAd = null
+                loadRewardAd()
+            }
+        }
+
+        _rewardAd?.show(this, OnUserEarnedRewardListener { rewardItem ->
+            var rewardAmount = rewardItem.amount
+            var rewardType = rewardItem.type
+            println("$rewardAmount $rewardType Awarded")
+        })
     }
 }
